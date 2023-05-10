1cb6598b793c7442e75f8c7082410829e10bfa80
sgudgkrfj <131796501+sgudgkrfj@users.noreply.github.com>
2023-05-11 01:19:14 +0300

    Четверте завдання

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-#3
-def ccc(*args):
-    d={}
-    for i in args:
-        d[i]=i**2
-    print(d)
-ccc(10,20,5)
+#4
+def get_odd_numbers(*args):
+    odd_numbers = []
+    for num in args:
+        if num % 2 != 0:
+            odd_numbers.append(num)
+    return odd_numbers
+result = get_odd_numbers(1, 2, 3, 4, 5, 6, 7, 8, 9)
+print(result)
